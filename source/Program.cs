d5287517f850295f173be219817b9f40c39ea1f9
SWAN <andrewswan89@gmail.com>
2022-07-03 12:04:10 +0700

    Метод печати массива и вывод данных

--- source/Program.cs
+++ source/Program.cs
@@ -22,3 +22,21 @@ void MetodNewArray (string [] array, string [] newarray)
             }
         }
 }
+//Метод печати массива
+void PrintArray (string [] array)
+    {
+        Console.Write("[ ");
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write($"{array[i]} ");
+        }
+        Console.Write("]");
+        Console.WriteLine();
+    }
+
+//InputArrayKeyboard(); // заполнение массива с клавиатуры
+Console.WriteLine("Заданый массив");
+PrintArray(Array);
+MetodNewArray(Array, NewArray);
+Console.WriteLine("Обработанный массив");
+PrintArray(NewArray);
